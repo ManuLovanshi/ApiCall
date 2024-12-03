8d2fac96ed3882a5f8014541f3ad14bd76ddfa13
Manu Lovanshi <lovanshimanu@gamil.com>
2024-12-03 22:13:56 +0530

    created /google endpoint

--- ApiCall/src/main/java/com/example/ApiCall/controller/BaseController.java
+++ ApiCall/src/main/java/com/example/ApiCall/controller/BaseController.java
@@ -1,5 +1,14 @@
 package com.example.ApiCall.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BaseController {
+	
+	@GetMapping("/google")
+	public String google() {
+		return "iojuyhgtrfd";
+	}
 
 }
